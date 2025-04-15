a0fade226901f8035ee257be70651f6c43ea3334
채근영 <chaulfe@gmail.com>
2025-04-15 12:20:41 +0900

    [Bronze II] Title: Hashing, Time: 32 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ Python/백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,10 @@
+l = int(input())
+alpabet = input()
+result = []
+res = 0
+for i in alpabet:
+    result.append(ord(i) - 96)
+for i in range(len(result)):
+    result[i] = result[i] * 31 ** i
+    res += result[i]
+print(res)
