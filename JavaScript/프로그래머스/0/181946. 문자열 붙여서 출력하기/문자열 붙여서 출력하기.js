1b87418041c5377eb05d3aef6f5640d5c00a8133
채근영 <chaulfe@gmail.com>
2025-09-01 01:50:14 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 298.49 ms, Memory: 31.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
+++ JavaScript/프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -2,10 +2,8 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-    console.log(input[0] + input[1])
-    rl.close()
+}).on('line', function (line) {
+    console.log(line.split(' ').join(""));
+    // spilt -> 공백으로 배열 분리
+    // join -> 배열을 빈 문자열로 합치기
 })
