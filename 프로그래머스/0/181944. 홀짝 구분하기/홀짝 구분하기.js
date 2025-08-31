9abbe978ab0b06f480e1d9afba9a24a474f90418
채근영 <chaulfe@gmail.com>
2025-09-01 03:48:38 +0900

    [level 0] Title: 홀짝 구분하기, Time: 44.89 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -2,15 +2,8 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    n = Number(input[0]);
-    if (n % 2 === 0) {
-        console.log(`${n} is even`);
-    } else console.log(`${n} is odd`);
-});
+}).on('line', function (line) {
+    n = +(line);
+    const res = n % 2 === 0 ? "even" : "odd";
+    console.log(`${n} is ${res}`);
+})
