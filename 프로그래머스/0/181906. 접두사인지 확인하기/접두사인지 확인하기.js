204ca6bb30fbf55f0745645e1d89cf9d8d792b6e
채근영 <chaulfe@gmail.com>
2025-09-01 03:10:24 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,5 +1,5 @@
 function solution(my_string, is_prefix) {
-    if (my_string.startsWith(is_prefix)) {
-        return 1
-    } else return 0
+    return +my_string.startsWith(is_prefix)
+    // startsWith -> 문자열이 접두사로 시작하는지 확인
+    // boolean 반환값에 + 추가하면 true = 1, false = 0으로 반환
 }
