efe1930c54730f70e04b0ed4e33919877eb4675f
채근영 <chaulfe@gmail.com>
2025-09-01 04:58:05 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,4 +1,5 @@
 function solution(str1, str2) {
-    const res = [...str1].map((a, b) => a+str2[b]);
+    const res = [...str1].map((currentValue, index) => currentValue+str2[index]);
+    // srt1의 currentValue와 str2[index]를 이은 값을 리턴한다
     return res.join("");
 }
