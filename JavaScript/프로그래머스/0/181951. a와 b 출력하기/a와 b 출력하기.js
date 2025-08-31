f0811b972e9e136b42b53c41338e68b51f7f8361
채근영 <chaulfe@gmail.com>
2025-09-01 03:12:57 +0900

    [level 0] Title: a와 b 출력하기, Time: 45.42 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line', function (line) {
+    input = line.split(' ');
+    console.log(`a = ${input[0]}`);
+    console.log(`b = ${input[1]}`);
+})
