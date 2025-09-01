9a003a8a068d531a3f5cb1629eb2d9d8c2fc99b1
채근영 <chaulfe@gmail.com>
2025-09-01 09:18:48 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ JavaScript/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,7 +1,3 @@
 function solution(a, b) {
-    let sum1 = Number(String(a)+String(b));
-    let sum2 = Number(String(b)+String(a));
-    if (sum1 >= sum2) {
-        return sum1
-    } else return sum2
+    return Math.max(Number(`${a}${b}`), Number(`${b}${a}`))
 }
